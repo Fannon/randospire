5864d06f1f52442ff0f377dd621401f2e5714196
Simon Heimler <mail@simon-heimler.de>
2023-02-03 08:07:24 +0100

    Init project structure

---
+++ src/cli.ts
@@ -0,0 +1,3 @@
+import { log } from './logger'
+
+log.info('TEST')
---
+++ src/logger.ts
@@ -0,0 +1,5 @@
+import { Logger } from 'tslog'
+
+export const log = new Logger({
+    prettyLogTemplate: '{{hh}}:{{MM}}:{{ss}} {{logLevelName}} ',
+})
