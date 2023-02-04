fe91661c73c5efe5c90e062e6b017367912d209f
Simon Heimler <mail@simon-heimler.de>
2023-02-04 12:38:34 +0100

    First MVP version

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,29 @@
  * Interface for the config files
  */
 export type Config = {
+    /**
+     * Name of result, will also result in a subfolder name
+     */
     name: string;
+    /**
+     * How many files to pick randomly
+     */
     amount: number;
-    inputFolders: string[]
-    fileExtensionRestrictions?: string[]
+    /**
+     * List of folders to search for files and randomly pick some
+     */
+    inputFolders: string[];
+    /**
+     * If not given, no file extension restriction is made (searching *.*)
+     */
+    fileExtensions?: string[];
+}[]
+
+/**
+ * Stores the results of the operation, so it is possible to understand what was done in retrospect
+ */
+export type Report = {
+    config: Config[];
+    inputFiles: any;
+    outputFiles: any;
 }[]
